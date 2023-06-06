5f5b899b4ee84ddc8c9f63d26b6815a8e63f26b4
Santiago Musso <santiagomusso@gmail.com>
2023-06-06 16:29:50 -0300

    Redeploy

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -9,8 +9,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  redirectURI: import.meta.env.VITE_REDIRECT_URI
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET
 }
 
 export const app = initializeApp(firebaseConfig)
